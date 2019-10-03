550a19ba521a37297687ef28f5f3175d5c229a07
vnikov <35333310+vnikov@users.noreply.github.com>
2019-10-03 17:28:59 +0800

    Add files via upload

---
+++ data/asyncio-examples/asyncio-old.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+
+log = logging.getLogger()
+
+def main():
+    loop = asyncio.get_event_loop()    
+    loop.run_until_complete(asyncio.gather(coroutine_1(), coroutine_2()))
+    
+    
+@asyncio.coroutine
+def coroutine_1():
+    log.info('coroutine_1 is active on the event loop')
+
+    log.info('coroutine_1 yielding control. Going to be blocked for 4 seconds')
+    yield from asyncio.sleep(4)
+
+    log.info('coroutine_1 resumed. coroutine_1 exiting')
+    
+
+@asyncio.coroutine
+def coroutine_2():
+    log.info('coroutine_2 is active on the event loop')
+
+    log.info('coroutine_2 yielding control. Going to be blocked for 5 seconds')
+    yield from asyncio.sleep(5)
+
+    log.info('coroutine_2 resumed. coroutine_2 exiting')
+    
+
+if __name__ == '__main__':
+    main()
